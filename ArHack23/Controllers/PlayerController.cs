dd3dcbae36eece7cea0334c3c1ec8612efdcdfb5
Ohad Bitton <ohbitton@microsoft.com>
2023-09-12 16:11:37 +0300

    log option

--- ArHack23/Controllers/PlayerController.cs
+++ ArHack23/Controllers/PlayerController.cs
@@ -1,6 +1,9 @@
 using ArHack23.Models;
 using ArHack23.Services;
+using Kusto.Data;
+using Kusto.Ingest;
 using Microsoft.AspNetCore.Mvc;
+using System.Text;
 using static ArHack23.Models.GameState;
 
 namespace ArHack23.Controllers;
@@ -9,8 +12,14 @@ namespace ArHack23.Controllers;
 [Route("Players")]
 public class PlayerController : ControllerBase
 {
+    private IKustoIngestClient cli;
+
     public PlayerController()
     {
+        var csb = new KustoConnectionStringBuilder("https://ohadev.westeurope.dev.kusto.windows.net/").WithAadApplicationKeyAuthentication("d5e0a24c-3a09-40ce-a1d6-dc5ab58dae66", "AfG8Q~6Yj4Wo4ZhIS8GdKThGRwc3THRFjM.Lva1R", "microsoft.com");
+        var ingest = new KustoConnectionStringBuilder("https://ingest-ohadev.westeurope.dev.kusto.windows.net/").WithAadApplicationKeyAuthentication("d5e0a24c-3a09-40ce-a1d6-dc5ab58dae66", "AfG8Q~6Yj4Wo4ZhIS8GdKThGRwc3THRFjM.Lva1R", "microsoft.com");
+
+        cli = KustoIngestFactory.CreateManagedStreamingIngestClient(csb, ingest);
     }
 
     [HttpGet]
@@ -83,4 +92,23 @@ public class PlayerController : ControllerBase
         GameService.SetFlags(flags);
         return NoContent();
     }
+
+    private void log(string rec)
+    {
+        using (var mem = new MemoryStream())
+        {
+            mem.Write(Encoding.UTF8.GetBytes(rec));
+            mem.Seek(0, SeekOrigin.Begin);
+            var res = cli.IngestFromStreamAsync(mem, new KustoIngestionProperties("ohtst", "Hack")).Result;
+            Console.WriteLine(res);
+        }
+    }
+
+    [HttpPost("/logs")]
+    public IActionResult AddLog(string log)
+    {
+        this.log(log);
+        return NoContent();
+    }
+
 }
